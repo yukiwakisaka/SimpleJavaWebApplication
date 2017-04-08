fef2537c8cf74387bc16ea6a4e751f750295c369
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2017-04-09 08:39:42 +0900

    modify result

--- src/main/java/DBAccessor.java
+++ src/main/java/DBAccessor.java
@@ -7,8 +7,8 @@ import java.util.Map;
  */
 public class DBAccessor {
 
-    public static Map<String, String> getUsers() {
-        Map<String, String> result = new HashMap<String, String>();
+    public static Map<Integer, String> getUsers() {
+        Map<Integer, String> result = new HashMap<Integer, String>();
 
         Connection conn = null;
         try {
@@ -17,8 +17,33 @@ public class DBAccessor {
 
             ResultSet rset = conn.createStatement().executeQuery("SELECT * FROM users");
             while (rset.next()) {
-                result.put(rset.getString(1), rset.getString(2));
+                result.put(rset.getInt(1), rset.getString(2));
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+    public static int setUser(int id, String name) {
+        int result = 0;
+
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/17training", "root", "");
+
+            result = conn.createStatement().executeUpdate(String.format("INSERT USERS VALUE(%d, '%s')", id, name));
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
--- src/main/java/GetServlet.java
+++ src/main/java/GetServlet.java
@@ -14,7 +14,7 @@ public class GetServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        Map<String, String> users = DBAccessor.getUsers();
+        Map<Integer, String> users = DBAccessor.getUsers();
 
         resp.setContentType("text/html");
 
