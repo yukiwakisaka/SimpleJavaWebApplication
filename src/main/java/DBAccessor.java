950e8fcab6a7be82142c11eaeaf70dee0ab9f54c
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2017-04-09 11:08:55 +0900

    get mysql url with getenv

--- src/main/java/DBAccessor.java
+++ src/main/java/DBAccessor.java
@@ -7,29 +7,29 @@ import java.util.Map;
  */
 public class DBAccessor {
 
-    public static Map<Integer, String> getUsers() throws ClassNotFoundException, SQLException {
-        Map<Integer, String> result = new HashMap<Integer, String>();
+    static final String MYSQL_URL = System.getenv().getOrDefault("MYSQL_URL", "jdbc:mysql://localhost:3306/17training?user=root&password=");
 
-        Connection conn = null;
+    public static Map<Integer, String> getUsers() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/17training", "root", "");
+        Connection conn = DriverManager.getConnection(MYSQL_URL);
 
         ResultSet rset = conn.createStatement().executeQuery("SELECT * FROM users");
+        Map<Integer, String> result = new HashMap<Integer, String>();
         while (rset.next()) {
             result.put(rset.getInt(1), rset.getString(2));
         }
         conn.close();
+
         return result;
     }
 
     public static int setUser(int id, String name) throws ClassNotFoundException, SQLException {
-        int result = 0;
-
         Class.forName("com.mysql.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/17training", "root", "");
+        Connection conn = DriverManager.getConnection(MYSQL_URL);
 
-        result = conn.createStatement().executeUpdate(String.format("INSERT USERS VALUE(%d, '%s')", id, name));
+        int result = conn.createStatement().executeUpdate(String.format("INSERT USERS VALUE(%d, '%s')", id, name));
         conn.close();
+
         return result;
     }
 }
