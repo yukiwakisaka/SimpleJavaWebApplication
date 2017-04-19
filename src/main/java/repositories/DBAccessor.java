5c3d8896c70a8ee2be21df6c63758b478e26b7fa
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2017-04-20 07:25:10 +0900

    add config.properties

--- src/main/java/repositories/DBAccessor.java
+++ src/main/java/repositories/DBAccessor.java
@@ -21,18 +21,18 @@ public class DBAccessor {
     }
 
     private static Connection getConnection() throws SQLException {
-        logger.info(String.format("New Connection: %s?user=%s&password=%s", URL, USER, PASSWORD));
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        logger.info("Start Connection: " + getUrl());
+        return DriverManager.getConnection(getUrl(), getUser(), getPassword());
     }
 
-    public Map<Integer, String> getUsers() throws ClassNotFoundException, SQLException {
+    public Map<Integer, String> getUsers() throws SQLException {
 
         //Class.forName("com.mysql.jdbc.Driver"); // javaのversionによって要らない
-        Map<Integer, String> result = new HashMap<Integer, String>();
+        Map<Integer, String> result = new HashMap<>();
 
         try (Connection conn = getConnection();
              Statement stmt = conn.createStatement();
-             ResultSet rset = stmt.executeQuery("SELECT * FROM users;")) {
+             ResultSet rset = stmt.executeQuery("SELECT * FROM USERS;")) {
             conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             while (rset.next()) {
                 result.put(rset.getInt(1), rset.getString(2));
@@ -41,7 +41,7 @@ public class DBAccessor {
         return result;
     }
 
-    public int setUser(int id, String name) throws ClassNotFoundException, SQLException {
+    public int setUser(int id, String name) throws SQLException {
 
         int result;
 
@@ -50,7 +50,6 @@ public class DBAccessor {
             conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             pstmt.setInt(1, id);
             pstmt.setString(2, name);
-            logger.info(pstmt.toString());
             result = pstmt.executeUpdate();
         }
         return result;
--- src/main/java/repositories/DBConfig.java
+++ src/main/java/repositories/DBConfig.java
@@ -4,10 +4,71 @@ package repositories;
  * @author yuki.wakisaka
  */
 class DBConfig {
-//    static final String URL = System.getenv().getOrDefault("MYSQL_URL", System.getProperty("mysql.url", ""));
-//    static final String USER = System.getenv().getOrDefault("MYSQL_USER", System.getProperty("mysql.user", ""));
-//    static final String PASSWORD = System.getenv().getOrDefault("MYSQL_PW", System.getProperty("mysql.password", ""));
-    static final String URL = System.getenv().getOrDefault("MYSQL_URL", System.getProperty("mysql.url", "jdbc:mysql://localhost:3306/17training"));
-    static final String USER = System.getenv().getOrDefault("MYSQL_USER", System.getProperty("mysql.user", "root"));
-    static final String PASSWORD = System.getenv().getOrDefault("MYSQL_PW", System.getProperty("mysql.password", ""));
+    private DBConfig(){};
+
+    private static final DBConf CONF = DBConf.get(System.getProperty("dbconf"));
+    private static final String RES = "config";
+
+    private static String url;
+    private static String user;
+    private static String password;
+
+    static {
+        switch (CONF) {
+            case ENV:
+                url = System.getenv().get("MYSQL_URL");
+                user = System.getenv().get("MYSQL_USER");
+                password = System.getenv().get("MYSQL_PASSWORD");
+                break;
+            case SYSTEM:
+                url = System.getProperty("mysql.url", "");
+                user = System.getProperty("mysql.uer", "");
+                password = System.getProperty("mysql.password", "");
+                break;
+            case RESOURCE:
+                url = java.util.ResourceBundle.getBundle(RES).getString("mysql.url");
+                user = java.util.ResourceBundle.getBundle(RES).getString("mysql.user");
+                password = java.util.ResourceBundle.getBundle(RES).getString("mysql.password");
+                break;
+            default:
+        }
+    }
+
+    public static String getUrl() {
+        return url;
+    }
+
+    public static String getUser() {
+        return user;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
+
+enum DBConf {
+    ENV("ENV"),
+    SYSTEM("SYS"),
+    RESOURCE("RES"),
+    DEFAULT("DEF");
+
+    String dbconf;
+
+    DBConf(String dbconf) {
+        this.dbconf = dbconf;
+    }
+
+    public static DBConf get(String dbconf) {
+        if (dbconf == null) return RESOURCE;
+
+        DBConf cf = DEFAULT;
+        for (DBConf c : DBConf.values()) {
+            if (dbconf.equals(c.name())) {
+                cf = c;
+                break;
+            }
+        }
+        return cf;
+    }
 }
